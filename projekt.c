15de2f325566aab381a4821fc0dc9ef287904f81
Sowiarz <pavel.horubala@gmail.com>
2014-06-14 10:28:18 +0200

    Pierwszy commit

---
+++ projekt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+int main (void)
+{
+	
+	
+	return 0;
+}
