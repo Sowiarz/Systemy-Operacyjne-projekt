6c532e759a23768ee70a128b46166e969e3c29d8
Sowiarz <pavel.horubala@gmail.com>
2014-06-14 10:45:10 +0200

    Napisanie maina

--- projekt.c
+++ projekt.c
@@ -6,9 +6,18 @@
 #include <sys/syscall.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
+pthread_t id_th[3];
 int main (void)
-{
-	
+{	
+	int i = 0;
+	pthread_create(&id_th[0], NULL, read, NULL);
+	pthread_create(&id_th[1], NULL, download, NULL);
+	pthread_create(&id_th[2], NULL, write, NULL);
+
+	for(i=0; i<3; i++)
+	{
+		pthread_join(id_th[i], NULL);
+	}
 	
 	return 0;
 }
